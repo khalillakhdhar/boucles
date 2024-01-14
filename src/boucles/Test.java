168ac7acb60db11cd442f916dc9a011b6d0104c2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-01-14 09:47:26 +0100

    Initial commit

---
+++ src/boucles/Test.java
@@ -0,0 +1,26 @@
+package boucles;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=12;
+		for(int i=2;i<=x;i++)
+		{
+			if(i%2==0)
+				System.out.println("i= "+i);
+		}
+		ArrayList<String> cars=new ArrayList<String>();
+cars.add("bmw");
+cars.add("Audi");
+cars.add("fiat");
+for(String car : cars)
+{
+	System.out.println(car);
+	
+}
+	}
+
+}
